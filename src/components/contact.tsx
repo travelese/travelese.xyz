3b6ae499a233fcd44571e15d8946933f64012d61
Armin Babaei <armin.babaei@me.com>
2024-05-03 07:46:30 -0400

    added footer link

---
+++ src/components/contact.tsx
@@ -0,0 +1,31 @@
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+export function Contact() {
+  return (
+    <div className="w-full py-12 md:py-24 lg:py-32 border-t">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+              Get in touch
+            </h2>
+            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              Have a project in mind? Let&apos;s discuss how we can bring your
+              vision to life.
+            </p>
+          </div>
+          <div className="w-full max-w-sm space-y-2">
+            <form className="flex flex-col gap-4">
+              <Input placeholder="Name" type="text" />
+              <Input placeholder="Email" type="email" />
+              <Textarea className="min-h-[100px]" placeholder="Message" />
+              <Button type="submit">Submit</Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 export function Footer() {
   return (
     <div className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 shadow border-t">
-      <p className="text-xs text-gray-500 dark:text-gray-400">
+      <Link
+        className="text-xs text-gray-500 dark:text-gray-400"
+        href="https://arminbabaeistudio.com"
+      >
         © 2024 Armin Babaei Studio. All rights reserved.
-      </p>
+      </Link>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link className="text-xs hover:underline underline-offset-4" href="#">
           Terms of Service
