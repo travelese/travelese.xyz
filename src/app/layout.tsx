c97b13b4a3a4b26bf6db66f2f19f9db7b6add3cb
Armin Babaei <armin.babaei@me.com>
2024-05-02 21:47:01 -0400

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider"
-import { MainNav } from "@/components/main-nav";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import "@/styles/globals.css";
 
+import { Inter as FontSans } from "next/font/google";
+
 import { cn } from "@/lib/utils";
+
+import { MainNav } from "@/components/main-nav";
 import { Footer } from "@/components/footer";
 
 const fontSans = FontSans({
