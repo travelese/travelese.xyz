f22660cf3acdce0a5e150b6515bb3a1d9b2ad4cc
Armin Babaei <armin.babaei@me.com>
2024-05-09 19:28:56 -0400

    ENG-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
-  env: {
-    NEXTAUTH_URL: process.env.VERCEL_URL
-      ? `${process.env.VERCEL_URL}/api/auth`
-      : "http://localhost:3000/api/auth",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
