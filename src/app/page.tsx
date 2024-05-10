f18cc579af2423dd0acc8cdbea4b829a99982987
Armin Babaei <armin.babaei@me.com>
2024-05-10 07:05:05 -0400

    adjusted page height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,14 @@ import {
 import FlightSearchForm from "@/components/flight-search-form";
 import HotelSearchForm from "@/components/hotel-search-form";
 
+export const metadata = {
+  title: "Travelese",
+  description: "Smartest way to book your next trip.",
+};
+
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-[100dvh]">
+    <div className="flex flex-col min-h-screen">
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
